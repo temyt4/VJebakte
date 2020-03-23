a50801d30d13454257ea95838cf1709d72c8a64b
xev1l <xev1l1th@gmail.com>
2020-03-23 19:02:30 +0500

    v1.0.1-pre-alpha(fixed userdto querry)

--- src/main/java/com/server/repos/UserRepo.java
+++ src/main/java/com/server/repos/UserRepo.java
@@ -4,12 +4,11 @@ import com.server.domain.User;
 import com.server.domain.dto.UserDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
 
-    @Query(value = "select new com.server.domain.dto.UserDto(u.id, u.username, u.password, u.birthdate, u.user_avatar, u.email, u.roles, u.friends, u.communities, u.messages) " +
-            "from User u left join u.roles r left join u.friends f left join u.communities c left join u.messages m " +
+    @Query("select new com.server.domain.dto.UserDto(u) " +
+            "from User u " +
             "where u.username =:username")
     UserDto findDtoByUsername(String username);
 
